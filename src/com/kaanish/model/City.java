f2c35b5a1451b30fe84d085df7b3bedede41b729
AM Infotrix <AM Infotrix@AMInfotrix-PC>
2015-11-27 12:43:52 +0530

    partial mapping

---
+++ src/com/kaanish/model/City.java
@@ -0,0 +1,59 @@
+package com.kaanish.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class City {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String cityName;
+
+	@ManyToOne
+	@JoinColumn(name = "stateId")
+	private State state;
+	
+	@OneToMany(mappedBy="city")
+	private List<Vendor> vendors;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public List<Vendor> getVendors() {
+		return vendors;
+	}
+
+	public void setVendors(List<Vendor> vendors) {
+		this.vendors = vendors;
+	}
+
+}
--- src/com/kaanish/model/Vendor.java
+++ src/com/kaanish/model/Vendor.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -20,10 +22,126 @@ public class Vendor {
 	private String address;
 	private String pinCode;
 	private String email;
-	
+
 	private String aliseName;
-	 
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date lastModifiedDate;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "venderTypeId")
+	private VendorType vendorType;
+
+	@ManyToOne
+	@JoinColumn(name = "cityId")
+	private City city;
+
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private Users users;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getPh1() {
+		return ph1;
+	}
+
+	public void setPh1(String ph1) {
+		this.ph1 = ph1;
+	}
+
+	public String getPh2() {
+		return ph2;
+	}
+
+	public void setPh2(String ph2) {
+		this.ph2 = ph2;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAliseName() {
+		return aliseName;
+	}
+
+	public void setAliseName(String aliseName) {
+		this.aliseName = aliseName;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public VendorType getVendorType() {
+		return vendorType;
+	}
+
+	public void setVendorType(VendorType vendorType) {
+		this.vendorType = vendorType;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	public Users getUsers() {
+		return users;
+	}
+
+	public void setUsers(Users users) {
+		this.users = users;
+	}
+
 }
