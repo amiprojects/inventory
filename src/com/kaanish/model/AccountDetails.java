99c388a0e8762d1b99ee515dab493529b87fa664
aminfotrix <aminfotrix@aminfotrix-PC>
2015-12-12 13:37:47 +0530

    a

--- src/com/kaanish/model/AccountDetails.java
+++ src/com/kaanish/model/AccountDetails.java
@@ -220,34 +220,27 @@ public class AccountDetails {
 		this.users = users;
 	}
 
-/*	@Override
+	/*
+	 * @Override public String toString() { return "{\"tax_Type_Group\":\"" +
+	 * tax_Type_Group.getId() + "\", " + "\"id\":\"" + id + "\", " +
+	 * "\"taxTotal\":\"" + tax_Type_Group.getTotalTaxValue() + "\"}"; }
+	 */
+
+	@Override
 	public String toString() {
-		return "{\"tax_Type_Group\":\"" + tax_Type_Group.getId() + "\", " + "\"id\":\"" + id + "\", "
-				+ "\"taxTotal\":\"" + tax_Type_Group.getTotalTaxValue() + "\"}";
-	}*/
+		return "{\"bankAccountNumber\":\"" + bankAccountNumber + "\", " + "\"bankChequeLable\":\"" + bankChequeLable
+				+ "\", " + "\"bankIFSCnumber\":\"" + bankIFSCnumber + "\", " + "\"bankMICRnumber\":\"" + bankMICRnumber
+				+ "\", " + "\"bankName\":\"" + bankName + "\", " + "\"bankRTGCnumber\":\"" + bankRTGCnumber + "\", "
+				+ "\"branch\":\"" + branch + "\", " + "\"cityId\":\"" + city.getId() + "\"city\":\""
+				+ city.getCityName() + "\", " + "\"cstNumber\":\"" + cstNumber + "\"," + "\"cstRegistrationDate\":\""
+				+ cstRegistrationDate + "\"," + "\"exciseRegistrationDate\":\"" + exciseRegistrationDate + "\","
+				+ "\"id\":\"" + id + "\"," + "\"panNumber\":\"" + panNumber + "\","
+				+ "\"serviceTaxRegistrationDate\":\"" + serviceTaxRegistrationDate + "\","
+				+ "\"serviceTaxRegistrationNumber\":\"" + serviceTaxRegistrationNumber + "\"," + "\"," + "\"users\":\""
+				+ users + "\"," + "\"vatNumber\":\"" + vatNumber + "\"," + "\"vatRegistrationDate\":\""
+				+ vatRegistrationDate + "\","+ "\"vendorId\":\"" + vendor.getId() + "\"vendorName\":\"" + vendor.getName() + "\"" + "\"tax_Type_Group\":\""
+				+ tax_Type_Group.getId() + "\", " + "\"id\":\"" + id + "\", " + "\"taxTotal\":\""
+				+ tax_Type_Group.getTotalTaxValue() + "\"}";
+	}
 
-		
-		@Override public String toString() { return
-		"{\"bankAccountNumber\":\"" + bankAccountNumber + "\", " +
-		"\"bankChequeLable\":\"" + bankChequeLable + "\", " +
-		"\"bankIFSCnumber\":\"" + bankIFSCnumber + "\", " +
-		"\"bankMICRnumber\":\"" + bankMICRnumber + "\", " + "\"bankName\":\""
-		+ bankName + "\", " + "\"bankRTGCnumber\":\"" + bankRTGCnumber +
-		"\", " + "\"branch\":\"" + branch + "\", " + "\"city\":\"" + city +
-		"\", " + "\"cstNumber\":\"" + cstNumber + "\"," +
-		"\"cstRegistrationDate\":\"" + cstRegistrationDate + "\"," +
-		"\"exciseRegistrationDate\":\"" + exciseRegistrationDate + "\"," +
-		"\"id\":\"" + id + "\"," + "\"panNumber\":\"" + panNumber + "\"," +
-		"\"serviceTaxRegistrationDate\":\"" + serviceTaxRegistrationDate +
-		"\"," + "\"serviceTaxRegistrationNumber\":\"" +
-		serviceTaxRegistrationNumber + "\"," + "\"tax_Type_Group\":\"" +
-		tax_Type_Group + "\"," + "\"users\":\"" + users + "\"," +
-		"\"vatNumber\":\"" + vatNumber + "\"," + "\"vatRegistrationDate\":\""
-		+ vatRegistrationDate + "\"," + "\"vendor\":\"" + vendor + "\""+
-		"\"tax_Type_Group\":\"" + tax_Type_Group.getId() + "\", " +
-		"\"id\":\"" + id + "\", "+ "\"taxTotal\":\"" +
-		tax_Type_Group.getTotalTaxValue() + "\"}" ;
-		}
-
-	
 }
