847fd47f7601e5a07d2e606e10d6a304016034ba
aminfotrix <aminfotrix@aminfotrix0>
2016-02-01 19:35:55 +0530

    tryin to do job assign

---
+++ src/com/kaanish/model/JobPlan.java
@@ -0,0 +1,7 @@
+package com.kaanish.model;
+
+import java.io.Serializable;
+
+public class JobPlan implements Serializable {
+
+}
---
+++ src/com/kaanish/model/JobberStock.java
@@ -0,0 +1,7 @@
+package com.kaanish.model;
+
+import java.io.Serializable;
+
+public class JobberStock implements Serializable {
+
+}
